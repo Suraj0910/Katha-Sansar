2c5883d1eed4a8619fcdb2b504ec265b47fa47a8
suraj0910 <gautamsuraj985@gmail.com>
2023-02-14 16:06:35 +0545

    Main Activity Created

--- app/src/main/java/com/example/kathaapp/MainActivity.java
+++ app/src/main/java/com/example/kathaapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.kathaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        ImageView btnst1 = findViewById(R.id.imgTitle);
+        ImageView btnSt2 = findViewById(R.id.buttonstory2);
+        ImageView btnSt3 = findViewById(R.id.buttonstory3);
+        ImageView btnSt4 = findViewById(R.id.buttonstory4);
+
+        btnst1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ca = new Intent(getApplicationContext(), ReadStory.class);
+                ca.putExtra("keyStory", "storyA");
+                startActivity(ca);
+
+            }
+        });
+        btnSt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ca = new Intent(getApplicationContext(), ReadStory.class);
+                ca.putExtra("keyStory", "storyB");
+                startActivity(ca);
+
+            }
+        });
+        btnSt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ca = new Intent(getApplicationContext(), ReadStory.class);
+                ca.putExtra("keyStory", "storyC");
+                startActivity(ca);
+
+            }
+        });
+        btnSt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ca = new Intent(getApplicationContext(), ReadStory.class);
+                ca.putExtra("keyStory", "storyD");
+                startActivity(ca);
+
+            }
+        });
+    }}
